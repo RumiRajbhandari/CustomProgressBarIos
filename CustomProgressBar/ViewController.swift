b441ec45ca5719533ce8678116c7ab71d1a172ac
rumi <rumi@Rumis-MacBook-Pro.local>
2018-12-13 14:51:18 +0545

    Initial Commit

---
+++ CustomProgressBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomProgressBar
+//
+//  Created by rumi on 12/13/18.
+//  Copyright © 2018 rumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
